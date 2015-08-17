c23954d725ac97bbe0b5d9ba148a34a6ecdf6f93
Alex Miller <quacksterr.15@gmail.com>
2015-08-18 02:04:49 +1000

    Adjusted wild zombie clothes

--- sqf/outbreak_code/config/cfgZombies/cfgZombieClothes.hpp
+++ sqf/outbreak_code/config/cfgZombies/cfgZombieClothes.hpp
@@ -19,8 +19,16 @@ class CfgClothes {
 	
 	wild[] = {
 		"U_IG_Guerilla3_1",
-		"U_IG_Guerilla3_2",
-		"U_C_HunterBody_grn"
+		"U_C_HunterBody_grn",
+		"U_C_HunterBody_brn",
+		"U_C_Poloshirt_blue",
+		"U_C_Poloshirt_burgundy",
+		"U_C_Poloshirt_stripped",
+		"U_C_Poloshirt_tricolour",
+		"U_C_Poloshirt_salmon",
+		"U_C_Poloshirt_redwhite",
+		"U_C_Commoner1_1",
+		"U_C_Poor_1"
 	};
 	
 	military[] = {
