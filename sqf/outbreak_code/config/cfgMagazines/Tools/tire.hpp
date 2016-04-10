7eae92a0a96e6bb1d9bd791f8f9b349fd0db187f
Alex Miller <quacksterr.15@gmail.com>
2016-04-10 21:38:54 +1000

    Added tire model back to tire item

--- sqf/outbreak_code/config/cfgMagazines/Tools/tire.hpp
+++ sqf/outbreak_code/config/cfgMagazines/Tools/tire.hpp
@@ -7,5 +7,6 @@ class tire: CA_Magazine
 				mass = 10;
 		}; 
 	picture = "\addons\outbreak_assets\textures\items\tire.paa";
+	model = "a3\structures_f\Civ\Garbage\Tyre_F.p3d";
 	descriptionShort = "";
 };
