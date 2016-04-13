d8c82634e2f7893e5fe49586d0785757d9fd7ae2
Alex Miller <quacksterr.15@gmail.com>
2016-04-13 21:08:00 +1000

    FN FAL/DMR magazine model

--- sources/outbreak_code/config/cfgMagazines/Ammunition/762x51.cpp
+++ sources/outbreak_code/config/cfgMagazines/Ammunition/762x51.cpp
@@ -5,10 +5,14 @@ class 20Rnd_762x51_DMR : CUP_20Rnd_762x51_DMR {
 	class cfgItemActions {
 		doubleClick = "['20Rnd_762x51_DMR', '20Rnd_762x51_FNFAL_M'] call fnc_convertMagazines;";
 	};
+	
+	model = "\A3\Structures_F_EPB\Items\Military\Magazine_rifle_F.p3d";
 };
 
 class 20Rnd_762x51_FNFAL_M : CUP_20Rnd_762x51_FNFAL_M {
 	class cfgItemActions {
 		doubleClick = "['20Rnd_762x51_FNFAL_M', '20Rnd_762x51_DMR'] call fnc_convertMagazines;";
 	};	
+	
+	model = "\A3\Structures_F_EPB\Items\Military\Magazine_rifle_F.p3d";
 };
