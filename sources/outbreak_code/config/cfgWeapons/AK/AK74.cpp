2498b8bb7861942663e7d755c88231070b45ba8b
Alex Miller <quacksterr.15@gmail.com>
2016-04-19 00:23:23 +1000

    Black magazine for AK74M

--- sources/outbreak_code/config/cfgWeapons/AK/AK74.cpp
+++ sources/outbreak_code/config/cfgWeapons/AK/AK74.cpp
@@ -2,5 +2,5 @@ class CUP_arifle_AK74;
 
 class MOD_AK74 : CUP_arifle_AK74 {
 	displayName = "AK74";
-	magazines[] = {"MOD_30Rnd_545x39"};
+	magazines[] = {"MOD_30Rnd_545x39", "MOD_30Rnd_545x39_M"};
 };
--- sources/outbreak_code/config/cfgWeapons/AK/AK74M.cpp
+++ sources/outbreak_code/config/cfgWeapons/AK/AK74M.cpp
@@ -2,5 +2,4 @@ class CUP_arifle_AK74M;
 
 class MOD_AK74M : CUP_arifle_AK74M {
 	displayName = "AK-74M";
-	magazines[] = {"MOD_30Rnd_545x39"};
 };
